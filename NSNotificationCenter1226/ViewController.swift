c02a18f3cd6aef68ae2db92914a23b012898efc8
Leslie <leslie.appl@gmail.com>
2020-12-26 14:41:54 +0800

    Initial Commit

---
+++ NSNotificationCenter1226/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NSNotificationCenter1226
+//
+//  Created by leslie on 12/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
